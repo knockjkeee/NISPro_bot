f38576898baa2d81339945e1b58ec3b42e24ec33
knockjkeee <knockjkeee@gmail.com>
2022-07-29 15:16:09 +0300

    update trigger.sql add new trigger for table article_data_mime (update 2 field [body article and subject article] to table telegram_receive_notification_new_article

--- SQL/trigger.sql
+++ SQL/trigger.sql
@@ -1,43 +1,49 @@
-DELIMITER //
-
 CREATE TRIGGER telegram_notification
     AFTER INSERT
     ON article
     FOR EACH ROW
 
 BEGIN
-
     SET @DYNAMIC_COUNT = (select COUNT(value_text)
                           from dynamic_field_value
-                          where object_id = (select ticket_id from article where article.id = NEW.id) and field_id = (select id from dynamic_field where name='Telegram'));
+                          where object_id = (select ticket_id from article where article.id = NEW.id)
+                            and field_id = (select id from dynamic_field where name = 'Telegram'));
 
     INSERT INTO telegram_receive_notification_new_article
-    (id_telegram, queue_id, tn, is_visible_for_customer, create_by, login_count_registration)
+    (id_telegram, queue_id, tn, is_visible_for_customer, create_by, login_count_registration, article_id)
     VALUES ((
                 CASE @DYNAMIC_COUNT
                     WHEN 0 THEN NULL
                     WHEN 1 THEN (select value_text
                                  from dynamic_field_value
-                                 where object_id = (select ticket_id from article where article.id = NEW.id) and field_id = (select id from dynamic_field where name='Telegram'))
+                                 where object_id = (select ticket_id from article where article.id = NEW.id)
+                                   and field_id = (select id from dynamic_field where name = 'Telegram'))
                     ELSE NULL
                     END),
             (select queue_id from ticket where id = (select ticket_id from article where id = NEW.id)),
             (select tn from ticket where id = (select ticket_id from article where id = NEW.id)),
             (select is_visible_for_customer from article where id = NEW.id),
             (select login from users where id = (select article.create_by from article where id = NEW.id)),
-            (select COUNT(login) from telegram_bot_registration where login=(select login from users where id = (select article.create_by from article where id = NEW.id))));
+            (select COUNT(login)
+             from telegram_bot_registration
+             where login =
+                   (select login from users where id = (select article.create_by from article where id = NEW.id))),
+            NEW.id);
 
 END;
-//
 
-DELIMITER ;
+CREATE TRIGGER update_telegram_notification
+    AFTER INSERT
+    ON article_data_mime
+    FOR EACH ROW
+
+BEGIN
+    UPDATE telegram_receive_notification_new_article
+    SET body = NEW.a_body, subject = NEW.a_subject
+    WHERE article_id = NEW.article_id;
+
+END;
 
 # show triggers;
-
-# select login from users where id=(select article.create_by from article where id = 709)
-
-# select count(value_text)  from dynamic_field_value where object_id = (select ticket_id from article where article.id = 777);
-
-# select count(ticket_id) from article where article.id = 777;
-
 # drop trigger telegram_notification;
+# drop trigger update_telegram_notification;
--- src/main/java/ru/newsystems/nispro_bot/base/model/db/TelegramReceiveNotificationNewArticle.java
+++ src/main/java/ru/newsystems/nispro_bot/base/model/db/TelegramReceiveNotificationNewArticle.java
@@ -19,21 +19,30 @@ public class TelegramReceiveNotificationNewArticle {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "tn", length = 100)
+    @Column(name = "tn")
     private String ticketNumber;
 
-    @Column(name = "id_telegram", length = 100)
+    @Column(name = "id_telegram")
     private String idTelegram;
 
-    @Column(name = "queue_id", length = 100)
+    @Column(name = "queue_id")
     private String queueId;
 
-    @Column(name = "is_visible_for_customer", length = 100)
+    @Column(name = "is_visible_for_customer")
     private Long isVisibleForCustomer;
 
-    @Column(name = "create_by", length = 100)
+    @Column(name = "create_by")
     private String createBy;
 
-    @Column(name = "login_count_registration", length = 100)
+    @Column(name = "login_count_registration")
     private Long loginCountRegistration;
+
+    @Column(name = "article_id")
+    private Long articleId;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "body", length = 99999999)
+    private String body;
 }
